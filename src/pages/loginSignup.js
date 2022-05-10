317e64877765449b7bc4b53175a825688a3a5322
nanni4287 <nanni.lee@aceenter.com>
2022-05-10 18:24:43 +0900

    sign up 완성 animation 효과 추가

--- src/pages/loginSignup.js
+++ src/pages/loginSignup.js
@@ -1,17 +1,60 @@
-import {LoginSignupBox, LoginSignupWrap, SignupButton, InputBox} from "../style/styleLoginSignup";
+import {LoginBox, SignupInputBox, LoginSignupWrap, SignupButton, InputBox, SignupBox, CloseButton} from "../style/styleLoginSignup";
+import {useState} from "react";
 
 function LoginSignup () {
+    const [isSignup, setIsSignup] = useState(false);
+
+    console.log(isSignup)
+
     return (
         <LoginSignupWrap>
-            <LoginSignupBox>
+            <LoginBox>
                 <h3>LOGIN</h3>
                 <InputBox>
-                    <input placeholder={"Username"}></input>
-                    <input type={"password"} placeholder={"Password"}></input>
+                    <input placeholder={"Nickname"} />
+                    <input type={"password"} placeholder={"Password"} />
                     <button>GO</button>
                 </InputBox>
-            </LoginSignupBox>
-            <SignupButton>SIGN UP</SignupButton>
+            </LoginBox>
+            <div className={isSignup ? "fadeInAnimation" : "fadeOutAnimation"}>
+                <SignupBox>
+                    <CloseButton onClick={() => setIsSignup(false)}>&times;</CloseButton>
+                    <h3>SIGNUP</h3>
+                    <SignupInputBox>
+                        <div id={"nicknameWrap"}>
+                            <input placeholder={"Nickname"} />
+                            <button>Confirm</button>
+                        </div>
+                        <input placeholder={"Phone Number"} />
+                        <input placeholder={"Email Address"} />
+                        <div id={"passwordWrap"}>
+                            <input type={"password"} placeholder={"Password"} />
+                            <input type={"password"} placeholder={"Repeat Password"} />
+                        </div>
+                        <button>GO</button>
+                    </SignupInputBox>
+                </SignupBox>
+            </div>
+            {isSignup ? null : <SignupButton onClick={() => setIsSignup(true)}>SIGN UP</SignupButton>}
+            {/*{isSignup ? <div className={"fadeInAnimation"}>*/}
+            {/*    <SignupBox>*/}
+            {/*        <CloseButton onClick={() => setIsSignup(false)}>&times;</CloseButton>*/}
+            {/*        <h3>SIGNUP</h3>*/}
+            {/*        <SignupInputBox>*/}
+            {/*            <div id={"nicknameWrap"}>*/}
+            {/*                <input placeholder={"Nickname"} />*/}
+            {/*                <button>Confirm</button>*/}
+            {/*            </div>*/}
+            {/*            <input placeholder={"Phone Number"} />*/}
+            {/*            <input placeholder={"Email Address"} />*/}
+            {/*            <div id={"passwordWrap"}>*/}
+            {/*                <input type={"password"} placeholder={"Password"} />*/}
+            {/*                <input type={"password"} placeholder={"Repeat Password"} />*/}
+            {/*            </div>*/}
+            {/*            <button>GO</button>*/}
+            {/*        </SignupInputBox>*/}
+            {/*    </SignupBox>*/}
+            {/*</div> : <SignupButton onClick={() => setIsSignup(true)}>SIGN UP</SignupButton>}*/}
         </LoginSignupWrap>
     )
 }
--- src/style/styleLoginSignup.js
+++ src/style/styleLoginSignup.js
@@ -8,12 +8,12 @@ export const LoginSignupWrap = styled.div`
     background-color : #E4DDE1;
 `;
 
-export const LoginSignupBox = styled.div`
+export const LoginBox = styled.div`
     width : 450px;
     height : 500px;
     border : 2px solid #B6AAAD;
     background-color : white;
-    border-radius : 20px;
+    border-radius : 10px;
     display : flex;
     flex-direction :column;
     justify-content : center;
@@ -23,7 +23,7 @@ export const LoginSignupBox = styled.div`
         height : 50px;
         font-size : 25px;
         line-height : 50px;
-        border-left : 6px solid #DF2059;
+        border-left : 6px solid #FF4F83;
     }
 `;
 
@@ -40,11 +40,11 @@ export const InputBox = styled.div`
     transition : 0.3s;
     padding-left : 10px;
     border-radius : 10px;
-    font-size : 15px;
+    font-size : 13px;
    }
    input:focus {
     outline : none;
-    border : 2px solid #DF2059;
+    border : 2px solid #FF4F83;
    }
    > button {
     height : 50px;
@@ -57,7 +57,7 @@ export const InputBox = styled.div`
     // color : white;
    }
    > button:hover {
-    background-color : #DF2059;
+    background-color : #FF4F83;
     color : white;
    }
 `;
@@ -65,7 +65,7 @@ export const InputBox = styled.div`
 export const SignupButton = styled.div`
     width : 100px;
     height : 100px;
-    background-color : #DF2059;
+    background-color : #FF4F83;
     position : absolute;
     left : 59%;
     top : 28%;
@@ -75,7 +75,7 @@ export const SignupButton = styled.div`
     line-height : 100px;
     font-size : 18px;
     cursor : pointer;
-    transition : 0.3s;
+    transition : 0.2s;
     :hover{
         width : 120px;
         height : 120px;
@@ -83,4 +83,111 @@ export const SignupButton = styled.div`
         line-height : 120px;
         font-size : 22px;
     }
+`;
+
+export const SignupBox = styled.div`
+    width : 450px;
+    height : 500px;
+    border : 2px solid #B6AAAD;
+    background-color : #FF4F83;
+    border-radius : 10px;
+    display : flex;
+    flex-direction :column;
+    justify-content : center;
+    align-items : center;
+    > h3{
+        width : 350px;
+        height : 50px;
+        font-size : 25px;
+        line-height : 50px;
+        border-left : 6px solid white;
+        color : white;
+    }
+`;
+
+export const CloseButton = styled.span`
+    color : white;
+    font-size : 50px;
+    position : absolute;
+    left : 88%;
+    top : 2%;
+    cursor : pointer;
+    transition : 0.5s;
+    width : 50px;
+    height : 50px;
+    text-align : center;
+    line-height : 40px;
+    :hover {
+        transform : rotate(-90deg)
+    }
+`;
+
+export const SignupInputBox =styled.div`
+   width : 350px;
+   display : flex;
+   flex-direction : column;
+   height : 300px;
+   margin : 0 auto;
+   > #nicknameWrap {
+    > button {
+        width : 80px;
+        height : 34px;
+        margin-left : 20px;
+        border-radius : 10px;
+        border : none;
+        background-color : white;
+        color : #FF4F83;
+        font-size : 12px;
+    }
+    > input{
+       height : 30px;
+       width : 230px;
+        border : 2px solid #ddd;
+        transition : 0.3s;
+        padding-left : 10px;
+        border-radius : 10px;
+        font-size : 13px;
+    }
+   }
+   > #passwordWrap {
+    display : flex;
+    margin-top : 20px;
+    > input {
+       width : 150px;
+       height : 30px;
+       border : 2px solid #ddd;
+       border-radius : 10px;
+       padding-left : 10px;
+       font-size : 13px;
+    }
+    > input:nth-child(2){
+        margin-left : 18px;
+    }
+   }
+   > input {
+    height : 30px;
+    margin-top : 20px;
+    border : 2px solid #ddd;
+    transition : 0.3s;
+    padding-left : 10px;
+    border-radius : 10px;
+    font-size : 13px;
+   }
+   input:focus {
+    outline : none;
+    border : 2px solid white;
+   }
+   > button {
+    height : 50px;
+    margin-top : 40px;
+    border : none;
+    border-radius : 10px;
+    background-color : white;
+    color : #FF4F83;
+    cursor : pointer;
+    transition : 0.3s;
+   }
+   > button:hover {
+    background-color : #FFEDF2;
+   }
 `;
