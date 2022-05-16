011255828a152a5cd8b1fbcabc18c24c1f806132
Lee Naeun <nanni4287@gmail.com>
2022-05-16 21:29:56 +0900

    [pageData.js] add data

--- src/data/pageData.js
+++ src/data/pageData.js
@@ -1,9 +1,25 @@
-import Main from "pages/main";
-import LoginSignup from "pages/loginSignup";
-import ProtectedRoute from "common/protectedRoute";
-import NavBar from "component/nav";
+import Main from 'pages/main';
+import LoginSignup from 'pages/loginSignup';
+import ProtectedRoute from 'common/protectedRoute';
+import NavBar from 'component/nav';
 
 export const routePageDatas = [
-    {routePath: '/', page : <LoginSignup />},
-    {routePath : '/main', page : <ProtectedRoute><NavBar /><Main /></ProtectedRoute>}
-]
+  { routePath: '/', page: <LoginSignup /> },
+  {
+    routePath: '/main',
+    page: (
+      <ProtectedRoute>
+        <NavBar />
+        <Main />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    routePath: '/mypage',
+    page: (
+      <ProtectedRoute>
+        <NavBar />
+      </ProtectedRoute>
+    ),
+  },
+];
